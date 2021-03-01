906400f0f324dc53602e47a46cf51caf9aa4f715
Chris Cann <canniman@googlemail.com>
2021-03-01 09:20:45 +0000

    new level minor tweaks and uploading itgit sts!

--- Assets/_Project/Scripts/Wowie/MainMenuController.cs
+++ Assets/_Project/Scripts/Wowie/MainMenuController.cs
@@ -13,17 +13,20 @@ namespace Wowie
 
         [SerializeField] private GameObject mainMenu;
         [SerializeField] private GameObject creditsMenu;
+        [SerializeField] private GameObject player;
 
 
         public void ToMainMenu()
         {
             creditsMenu.SetActive(false);
             mainMenu.SetActive(true);
+            player.SetActive(true);
         }
 
         public void ToCreditsMenu()
         {
             mainMenu.SetActive(false);
+            player.SetActive(false);
             creditsMenu.SetActive(true);
         }
     }
